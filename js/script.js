91114fc3c7468d5d5eee7d286e4f917078d9d1a9
Foysal Hasib <118272136+foysalhasib@users.noreply.github.com>
2024-01-23 22:13:35 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+var a=100;
+var b=50;
+let c=a+b;
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+
+// ========================Addition(plus)======================
+var x=20;
+var y=10;
+var z= x+y;
+
+console.log(z)
+
+
+
+// ========================Subtraction(minus)==================
+var x=20;
+var y=10;
+var z= x-y;
+
+console.log(z)
+
+
+// ========================Multiplication(Gun)=================
+var x=20;
+var y=10;
+var z= x*y;
+
+console.log(z)
+// ==========================Division(Vag)=====================
+var x=20;
+var y=10;
+var z= x/y;
+
+console.log(z)
+
+
+
+
